a79199689df109ccdacf5c020913be24adfcc5bc
Don Sheehy <don.r.sheehy@gmail.com>
2020-04-01 22:32:00 -0400

    Version bump for 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ds2",
-    version="0.2.0",
+    version="0.2.1",
     author="Donald R. Sheehy",
     author_email="don.r.sheehy@gmail.com",
     description="Don Sheehy's Data Structures",
